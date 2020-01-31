dde5e0f5c09defbd0247b5c607ad0bf2e1ad7f8d
zqqpluto <1793327987@qq.com>
2020-01-31 16:37:38 +0800

    Merge branch 'master' of https://github.com/zqqpluto/medicinal
    # Conflicts:
    #	src/main/resources/application.properties

--- src/main/java/cn/pluto/medicinal/MedicinalApplication.java
+++ src/main/java/cn/pluto/medicinal/MedicinalApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MedicinalApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MedicinalApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MedicinalApplication.class, args);
+	}
 
 }
